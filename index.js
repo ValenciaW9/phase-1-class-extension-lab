a75d75325d35a8459af6572fb2e9a1bb4226bf41
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-10-07 00:38:20 -0400

    phase-1-class-extension-lab/
    phase-1-class-extension-lab/

--- index.js
+++ index.js
@@ -3,22 +3,45 @@ class Polygon {
     this.sides = sides;
   }
 
+  // Use the get keyword to create a getter method countSides
   get countSides() {
     return this.sides.length;
   }
 
+  // Use the get keyword to create a getter method perimeter
   get perimeter() {
     return this.sides.reduce((sum, side) => sum + side, 0);
   }
 }
-
 class Triangle extends Polygon {
+  // Use the get keyword to create a getter method isValid
   get isValid() {
     if (this.countSides !== 3) {
       return false;
     }
 
-    const [side1, side2, side3] = this.sides;
-    return (side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1);
+    const [a, b, c] = this.sides;
+    return a + b > c && a + c > b && b + c > a;
+  }
+}
+class Square extends Polygon {
+  // Use the get keyword to create a getter method isValid
+  get isValid() {
+    if (this.countSides !== 4) {
+      return false;
+    }
+
+    const [a, b, c, d] = this.sides;
+    return a === b && b === c && c === d;
+  }
+
+  // Use the get keyword to create a getter method area
+  get area() {
+    if (!this.isValid) {
+      return "Invalid square";
+    }
+
+    const [side] = this.sides;
+    return side * side;
   }
 }
