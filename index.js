15b023752a85365564cc2cd8d005a840f7399327
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-10-06 23:39:34 -0400

    phase-1-class-extension-lab/
    phase-1-class-extension-lab/

--- index.js
+++ index.js
@@ -14,31 +14,22 @@ class Polygon {
 
 class Triangle extends Polygon {
   get isValid() {
-    if (this.countSides !== 3) {
-      return false;
-    }
-
     const [side1, side2, side3] = this.sides;
-    return side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1;
+    return (
+      side1 + side2 > side3 &&
+      side1 + side3 > side2 &&
+      side2 + side3 > side1
+    );
   }
 }
 
 class Square extends Polygon {
   get isValid() {
-    if (this.countSides !== 4) {
-      return false;
-    }
-
-    const [side1, side2, side3, side4] = this.sides;
-    return side1 === side2 && side1 === side3 && side1 === side4;
+    return this.sides.every(side => side === this.sides[0]);
   }
 
   get area() {
-    if (this.countSides !== 4) {
-      return undefined;
-    }
-
-    const [side] = this.sides;
+    const side = this.sides[0];
     return side * side;
   }
 }
